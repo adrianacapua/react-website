d2ddec12446630609d26796d5e08151d317d8aed
adrianacapua <adrianajcapua@gmail.com>
2022-10-02 22:38:40 +0800

    feat: initialized project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: white;
+`;
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Container>
+      <Navbar />
+      <Intro />
+    </Container>
+  )
 };
 
 export default App;
